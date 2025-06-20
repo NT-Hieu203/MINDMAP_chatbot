db23e28684343255e682454bd7ea1a527658b608
hieu10-06 <hieu10nguyen06@gmail.com>
2025-06-20 16:46:12 +0700

    update frontend

--- front_end/web_frontend/src/App.vue
+++ front_end/web_frontend/src/App.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class = 'app-container'>
-    <ChatBox />
+  <div id="app">
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import ChatBox from './components/ChatBox.vue';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 export default {
-  components: { ChatBox }
+  name: 'App'
 };
-
 </script>
-<style>
 
+<style>
+/* CSS toàn cục */
+body {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: #eef2f8;
+  color: #333;
+  line-height: 1.6;
+}
+
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  /* Để các component không bị giãn ra toàn bộ chiều cao */
+  min-height: 100vh;
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+/* Các style chung cho card, button... có thể đặt ở đây hoặc trong từng component */
 </style>
